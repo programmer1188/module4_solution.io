0b4a95396705d1f6958043ee60d0cb7a156bc433
programmer1188 <madhusudhanareddy.2016@vitstudent.ac.in>
2021-03-01 11:14:18 +0530

    final1

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-
-(function() {
-
+(function (window) {
     var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-    
-    for (var i = 0; i < names.length; i++) {
+  
+    for (var i in names) {
       var firstLetter = names[i].charAt(0).toLowerCase();
       if (firstLetter === 'j') {
-        byeSpeaker.speak(names[i]);
+        byeSpeaker(names[i]);
       } else {
-        helloSpeaker.speak(names[i]);
+        helloSpeaker(names[i]);
       }
     }
-    
-})();
-
+  })(window);
+  
+  
